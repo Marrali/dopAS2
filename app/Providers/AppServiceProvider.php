a594428ab2465b20f169d8edc641613c0a627266
Dk Maryam <23FTT1189@student.pb.edu.bn>
2025-03-23 17:10:47 +0800

    added logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +21,15 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
-    }
+{
+    VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+        return (new MailMessage)
+            ->subject('Verify Your Email Address')
+            ->line('Click the button below to verify your email address.')
+            ->line('If you did not create an account, no further action is required.')
+            ->line('<img src="' . asset('pblogo.png') . '" alt="PB Logo" style="width: 150px;">') // Add logo here
+            ->action('Verify Email Address', $url)
+            ->salutation('Best regards, <br> Dop AS2 Maryam');
+    });
+}
 }
