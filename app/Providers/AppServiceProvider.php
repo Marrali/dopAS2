3fdbc4bcdb9180b66c207a6e974274f930b99c46
Dk Maryam <23FTT1189@student.pb.edu.bn>
2025-03-23 17:20:20 +0800

    tukar logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\HtmlString;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,11 +27,13 @@ class AppServiceProvider extends ServiceProvider
     VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
         return (new MailMessage)
             ->subject('Verify Your Email Address')
+            ->greeting('Hello!')
             ->line('Click the button below to verify your email address.')
-            ->line('If you did not create an account, no further action is required.')
-            ->line('<img src="' . asset('pblogo.png') . '" alt="PB Logo" style="width: 150px;">') // Add logo here
             ->action('Verify Email Address', $url)
-            ->salutation('Best regards, <br> Dop AS2 Maryam');
+            ->line('If you did not create an account, no further action is required.')
+            ->line(new HtmlString('<img src="' . asset('pblogo.png') . '" alt="PB Logo" style="width: 150px; margin-top: 10px;">'))
+            ->salutation(new HtmlString('Best regards, <br> Dop AS2 Maryam'));
     });
 }
+
 }
